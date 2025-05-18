de42d900a8c8f779647ee3570a608d4bdfe703f4
dushyantyadav2005 <dyadav15112005@gmail.com>
2025-05-18 10:55:33 +0530

    message-route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
+// import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.models.js"
+import cloudinary from "../lib/cloudinary.js";
 
 import bcrypt from "bcryptjs";
 
@@ -86,43 +88,36 @@ export const logout=(req,res)=>{
 };
 
 export const UpdateProfile=async(req,res)=>{
-   const {email,password,fullName,profilePic}=req.body;
+
    try{
-          if(password.length<6)
-          {
-            return res.status(400).json({message:"Password must be at least 6 characters"});
-          }
-          const user=await User.findOne({email});
-          const salt=await bcrypt.genSalt(10);
+         const {profilePic}=req.body;
+         const userId=req.user._id;
 
-          const hashedPassword=await bcrypt.hash(password,salt);
+         if(!profilePic)
+         {
+          return req.status(400).json({message:"profile pic requird"});
+         }
 
-          const newUser=new User({
-            fullName,
-            email,
-            password:hashedPassword
-          })
+        const uploadResponse= await cloudinary.uploader.upload(profilePic);
+        const updatedUser=await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true});
 
-          if(newUser)
-          {
-                generateToken(newUser._id,res);
-                await newUser.save();
-
-                res.status(200).json({
-                    _id:newUser._id,
-                    fullName:newUser.fullName,
-                    email:newUser.email,
-                    profilePic:newUser.profilePic,
-                });
-          }else{
-            res.status(400).json({message:"Invalid user data"});
-        }
-
+        Response.status(200).json(updatedUser);
    }catch(err)
    {
     res.status(500),json({message:"Internal Server error"});
    }
 };
+
+
+export const checkAuth=(req,res)=>{
+    try{
+        res.status(200).json(req.user);
+    }catch(err)
+    {
+        console.error("Error in checkAuth controller ",err.message);
+        res.status(500).json({message:"Internal server error"});
+    }
+}
 
 
 
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 
-import {login,logout,signup,UpdateProfile} from "../controllers/auth.controller.js"
+import {login,logout,signup,UpdateProfile,checkAuth} from "../controllers/auth.controller.js"
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router =express.Router();
 
@@ -9,6 +10,8 @@ router.post("/signup",signup)
 router.post("/login",login)
 router.post("/logout",logout)
 router.put("/update-profile",protectRoute,UpdateProfile)
+
+router.get("/check",protectRoute,checkAuth)
 
 export default router;
 
